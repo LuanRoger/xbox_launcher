a39fe1f588dc0db5a21ea0379713004d5e705088
LuanRoger <luan.roger.2003@gmail.com>
2022-10-11 10:48:44 -0300

    Init sys_info_package

---
+++ internals/sys_info_getter/lib/sys_info_getter.dart
@@ -0,0 +1,11 @@
+library sys_info_getter;
+
+export 'src/controllers/terminal_helper.dart';
+
+export 'src/enums/get_error.dart';
+
+export 'src/models/disk_info.dart';
+export 'src/models/system_informations.dart';
+
+export 'src/services/models/providers/powershell_provider.dart';
+export 'src/services/models/providers/terminal_provider.dart';
